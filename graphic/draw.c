e2bbbf8d287e84dbe9e6de25d8d32d8eb165d56d
douzhk <douzhk@nationalchip.com>
2016-08-03 00:00:44 +0800

    图片添加，在主界面和关于界面

--- graphic/draw.c
+++ graphic/draw.c
@@ -3,12 +3,19 @@
 
 void start_game(void);
 void game_about(void);
+static void image_play(GtkWidget *frame, const char *url, int size_x, int size_y, int x, int y);
+
+static const char *src_image[2] = {"resource/main_surface.png", "resource/about.jpg"};
+
+GtkWidget *window, *frame;
+GtkWidget *exit_button, *ack_button;
+GtkWidget *image;
+
+#define WINDOW_X 400
+#define WINDOW_Y 400
 
 void test(void)
 {
-	GtkWidget *window, *frame;
-	GtkWidget *exit_button, *ack_button;
-
 	int argc = 0;
 	char **argv = NULL;
 
@@ -18,7 +25,7 @@ void test(void)
 		printf("window new failed\n");
 		return ;
 	}
-	gtk_widget_set_usize(window, 400, 400);
+	gtk_widget_set_usize(window, WINDOW_X, WINDOW_Y);
 	gtk_widget_set_uposition(window, 100, 100);
 	gtk_window_set_title(GTK_WINDOW(window), "五子棋");
 
@@ -31,13 +38,7 @@ void test(void)
 	gtk_container_add(GTK_CONTAINER(window), frame);
 
 	/* 背景图片 */
-//	gtk_image_new_from_file();
-
-	/* 标签，设置文本信息 */
-//	GtkWidget *label;
-//	label = gtk_label_new("1、开始游戏\n2、关于");
-//	//gtk_label_set_text(label, "lala");
-//	gtk_fixed_put(GTK_FIXED(frame), label, 150, 100);
+	image_play(frame, src_image[0], WINDOW_X, WINDOW_Y, 0, 0);
 
 	/* button */
 	ack_button  = gtk_button_new_with_label("开始游戏");
@@ -66,10 +67,64 @@ show:
 
 void start_game(void)
 {
-	;
+	gtk_container_remove(GTK_CONTAINER(window), frame);
+
+	// 画棋盘
+	GdkPixmap *pixmap = NULL;
+	GdkGC *gc;
+
+	gc = gdk_gc_new(window->window);
+	pixmap = gdk_pixmap_new(window->window,
+			WINDOW_X,
+			WINDOW_Y,
+			-1);
+	gdk_draw_rectangle (pixmap,
+			gc,
+			TRUE,
+			0, 0,
+			WINDOW_X,
+			WINDOW_Y);
+	gtk_widget_show_all(window);
 }
+
+
+static const char *about_title = "关   于";
+static const char *about_content = "作者: 窦中康\n\n源码地址: https://github.com/dzkcoming/Little_Game\n\n加入我们: 来吧，加入我们的队伍";
 
 void game_about(void)
 {
-	;
+	gtk_container_remove(GTK_CONTAINER(window), frame);
+
+	GtkWidget *fixed;
+	GtkWidget *label_title, *label_content;
+	fixed = gtk_fixed_new();
+	if (fixed == NULL) {
+		printf("fixed new failed\n");
+	}
+	gtk_container_add(GTK_CONTAINER(window), fixed);
+
+	image_play(fixed, src_image[1], WINDOW_X, WINDOW_Y, 0, 0);
+
+	label_title   = gtk_label_new(about_title);
+	label_content = gtk_label_new(about_content);
+	gtk_fixed_put(GTK_FIXED(fixed),   label_title,  180, 0);
+	gtk_fixed_put(GTK_FIXED(fixed), label_content, 0, 80);
+
+
+	gtk_widget_show_all(window);
+}
+
+static void image_play(GtkWidget *frame, const char *url, int size_x, int size_y, int x, int y)
+{
+	//5.通过GdkPixbuf来写该图片大小
+	//5.1创建pixbuf，需要占用资源，使用完需要释放
+	GdkPixbuf *src = gdk_pixbuf_new_from_file(url, NULL);
+	//5.2修改图片大小
+	GdkPixbuf *dst = gdk_pixbuf_scale_simple(src, size_x, size_y, GDK_INTERP_BILINEAR);
+	GtkWidget *image = gtk_image_new_from_pixbuf(dst);
+	//5.3pixbuf使用完需要释放其中资源
+	g_object_unref(src);
+	g_object_unref(dst);
+
+	gtk_fixed_put(GTK_FIXED(frame), image, 0, 0);
 }
