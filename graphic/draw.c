1fc1e36697ebaa65c5b63911720996b1012d3519
douzhk <douzhk@nationalchip.com>
2016-08-05 00:24:49 +0800

    绘制棋子，添加输赢判断

--- graphic/draw.c
+++ graphic/draw.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <string.h>
 
 void start_game(void);
 void game_about(void);
 static void image_play(GtkWidget *frame, const char *url, int size_x, int size_y, int x, int y);
 
-static const char *src_image[2] = {"resource/main_surface.png", "resource/about.jpg"};
+typedef struct
+{
+	gint x;
+	gint y;
+}RECT_POINTER;
+
+static const char *src_image[3] = {"resource/main_surface.png", "resource/about.jpg", "resource/game_background.jpg"};
 
 GtkWidget *window, *frame;
 GtkWidget *exit_button, *ack_button;
@@ -13,6 +20,51 @@ GtkWidget *image;
 
 #define WINDOW_X 400
 #define WINDOW_Y 400
+#define LINE_SPACING  20
+#define ROW_SPACING   20
+#define CHESS_START_X 20
+#define CHESS_START_Y 20
+#define LINE_NUM ((WINDOW_Y - 2 * CHESS_START_Y) / LINE_SPACING + 1)
+#define ROW_NUM  ((WINDOW_X - 2 * CHESS_START_X) / ROW_SPACING + 1)
+
+char chess_map[LINE_NUM][ROW_NUM] = {{0}, };
+char chess_black[LINE_NUM][ROW_NUM] = {{0}, };
+char chess_white[LINE_NUM][ROW_NUM] = {{0}, };
+
+/*
+ * return : 1 : win, 0 : not win
+ */
+int is_win(int is_black)
+{
+	char (*p)[ROW_NUM];
+	int i, j;
+
+	if (is_black)
+		p = chess_black;
+	else
+		p = chess_white;
+
+	for (i = 0; i < LINE_NUM; i++) {
+		for (j = 0; j < ROW_NUM; j++) {
+			if (p[i][j] != 0) {
+				if ((ROW_NUM - j >= 5) && (LINE_NUM - i >= 5)) {
+					if (p[i][j] && p[i + 1][j + 1] && p[i + 2][j + 2] && p[i + 3][j + 3] && p[i + 4][j + 4])
+						goto win;
+				} else if (ROW_NUM - j >= 5) {
+					if (p[i][j] && p[i][j + 1] && p[i][j + 2] && p[i][j + 3] && p[i][j + 4])
+						goto win;
+				} else {
+					if (p[i][j] && p[i + 1][j] && p[i + 2][j] && p[i + 3][j] && p[i + 4][j])
+						goto win;
+				}
+			}
+		}
+	}
+
+	return 0;
+win:
+	return 1;
+}
 
 void test(void)
 {
@@ -65,25 +117,137 @@ show:
 	return ;
 }
 
+void draw_chessboard(GtkWidget *wd)
+{
+	GdkColor color;
+	GdkGC *gc;
+	GdkSegment seg[LINE_NUM + ROW_NUM];
+	int i;
+
+	gc = gdk_gc_new(wd->window);
+	color.red   = 0x0000;
+	color.green = 0x0000;
+	color.blue  = 0xffff;
+	gdk_gc_set_rgb_fg_color(gc, &color);
+	gdk_draw_rectangle(wd->window, gc, TRUE, 0, 0, WINDOW_X, WINDOW_Y);
+
+
+	color.red   = 0x0000;
+	color.green = 0x0000;
+	color.blue  = 0x0000;
+	gdk_gc_set_rgb_fg_color(gc, &color);
+
+	for (i = 0; i < LINE_NUM; i++) {
+		seg[i].x1 = 20;
+		seg[i].y1 = LINE_SPACING * (i + 1);
+		seg[i].x2 = WINDOW_X - 20;
+		seg[i].y2 = LINE_SPACING * (i + 1);
+	}
+	for (; i < LINE_NUM + ROW_NUM; i++) {
+		seg[i].x1 = ROW_SPACING * ((i - LINE_NUM) + 1);
+		seg[i].y1 = 20;
+		seg[i].x2 = ROW_SPACING * ((i - LINE_NUM) + 1);
+		seg[i].y2 = WINDOW_Y - 20;
+	}
+
+//	printf("============= line : %d row : %d ================\n", LINE_NUM, ROW_NUM);
+
+	gdk_draw_segments(wd->window, gc, seg, LINE_NUM + ROW_NUM);
+}
+
+#define TO_ROW(x)  (x / 20 - 1)
+#define TO_LINE(x) (x / 20 - 1)
+static void draw_start(GtkWidget *draw,GdkEventButton *event,RECT_POINTER *rect)
+{
+	GdkColor color;
+	static GdkGC *gc, *gc_black, *gc_white, *gc_red;
+	static int inited;
+	static int is_black = 1;
+	char *black_win = "Congratulation , black is win";
+	char *white_win = "Congratulation , white is win";
+	int x, y;
+
+	if (inited == 0) {
+		gc_black = gdk_gc_new(draw->window);
+		color.red   = 0x0;
+		color.green = 0x0;
+		color.blue  = 0x0;
+		gdk_gc_set_rgb_fg_color(gc_black, &color);
+
+		gc_white = gdk_gc_new(draw->window);
+		color.red   = 0xffff;
+		color.green = 0xffff;
+		color.blue  = 0xffff;
+		gdk_gc_set_rgb_fg_color(gc_white, &color);
+
+		gc_red = gdk_gc_new(draw->window);
+		color.red   = 0xffff;
+		color.green = 0x0000;
+		color.blue  = 0x0000;
+		gdk_gc_set_rgb_fg_color(gc_red, &color);
+	}
+
+	if (is_black)
+		gc = gc_black;
+	else
+		gc = gc_white;
+
+	// 灵敏
+	if (((int)event->x % 20) <= 10 && ((int)event->y % 20) <= 10)
+		;
+	else
+		return ;
+
+	if ((event->x - 10 > 0) && (event->y - 10 > 0)) {
+		x = (int)event->x / 20 * 20;
+		y = (int)event->y / 20 * 20;
+	} else
+		x = y = 0;
+
+
+	printf("================ line %d row %d ===================\n", TO_LINE(y), TO_ROW(x));
+
+	gdk_draw_arc(draw->window, gc, TRUE, x - 10, y - 10, 20, 20, 0, 360 * 64);
+
+	chess_map[TO_LINE(y)][TO_ROW(x)] = 1;
+	if (is_black) {
+		chess_black[TO_LINE(y)][TO_ROW(x)] = 1;
+		if (is_win(1))
+			gdk_draw_text(draw->window, NULL, gc_red, 160, 160, black_win, strlen(black_win));
+	} else {
+		chess_white[TO_LINE(y)][TO_ROW(x)] = 1;
+		if (is_win(0))
+			gdk_draw_text(draw->window, NULL, gc_red, 160, 160, white_win, strlen(white_win));
+	}
+
+
+	is_black = !is_black;
+}
+
+static gboolean my_expose(GtkWidget      *da,
+		GdkEventExpose *event,
+		gpointer        data )
+{
+	RECT_POINTER rect;
+
+	draw_chessboard(da);
+	gtk_widget_add_events(da, GDK_BUTTON_PRESS_MASK);
+	g_signal_connect(G_OBJECT(da), "button_press_event", G_CALLBACK(draw_start), &rect);
+
+	gtk_widget_show_all(da);
+//	return FALSE;
+}
+
 void start_game(void)
 {
 	gtk_container_remove(GTK_CONTAINER(window), frame);
 
-	// 画棋盘
-	GdkPixmap *pixmap = NULL;
-	GdkGC *gc;
+	GtkWidget *da;
 
-	gc = gdk_gc_new(window->window);
-	pixmap = gdk_pixmap_new(window->window,
-			WINDOW_X,
-			WINDOW_Y,
-			-1);
-	gdk_draw_rectangle (pixmap,
-			gc,
-			TRUE,
-			0, 0,
-			WINDOW_X,
-			WINDOW_Y);
+	da = gtk_drawing_area_new ();
+	gtk_container_add(GTK_CONTAINER(window), da);
+    g_signal_connect(da, "expose_event", G_CALLBACK(my_expose), NULL );
+
 	gtk_widget_show_all(window);
 }
 
