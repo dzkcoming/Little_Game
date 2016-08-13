25431deb7f9427c77e514d18cf547c3f22260120
douzhk <douzhk@nationalchip.com>
2016-08-14 00:10:28 +0800

    增加电脑

--- graphic/draw.c
+++ graphic/draw.c
@@ -26,7 +26,7 @@ GtkWidget *image;
 #define CHESS_START_Y 20
 #define LINE_NUM ((WINDOW_Y - 2 * CHESS_START_Y) / LINE_SPACING + 1)
 #define ROW_NUM  ((WINDOW_X - 2 * CHESS_START_X) / ROW_SPACING + 1)
-#define IS_VALID(p) (p == 0 ? 0 : 1)
+#define IS_VALID_COOR(i, j) (i < ROW_NUM && j < LINE_NUM)
 
 typedef enum {
 	RIGHT      = 0x1,
@@ -39,11 +39,257 @@ char chess_map[LINE_NUM][ROW_NUM] = {{0}, };
 char chess_black[LINE_NUM][ROW_NUM] = {{0}, };
 char chess_white[LINE_NUM][ROW_NUM] = {{0}, };
 
-int get_next_piece_direction(char (*p)[ROW_NUM])
+piece_direction get_next_piece_direction(const char (*p)[ROW_NUM], int *get_row, int *get_line, int *len)
 {
+	piece_direction ret = RIGHT, ret1;
+	piece_direction tmp;
+
+	int i, j, i1, j1;
+	int max_len, max_len1, cur_len;
+
+	max_len = max_len1 = 0;
+	for (i = 0; i < LINE_NUM; i++) {
+		for (j = 0; j < ROW_NUM; j++) {
+			max_len1 = max_len;
+			if (p[i][j] != 0) {
+
+				tmp = RIGHT;
+				cur_len = 1;
+				i1 = i; j1 = j;
+				while (IS_VALID_COOR(i1, j1 + 1) && p[i1][j1 + 1] == 1) {
+					cur_len++;
+					j1++;
+				}
+
+				if (cur_len > max_len1) {
+					max_len1 = cur_len;
+					ret1 = tmp;
+				}
+
+				tmp = DOWN;
+				cur_len = 1;
+				i1 = i; j1 = j;
+				while (IS_VALID_COOR(i1 + 1, j1) && p[i1 + 1][j1] == 1) {
+					cur_len++;
+					i1++;
+				}
+
+				if (cur_len > max_len1) {
+					max_len1 = cur_len;
+					ret1 = tmp;
+				}
+
+
+				tmp = RIGHT_DOWN;
+				cur_len = 1;
+				i1 = i; j1 = j;
+				while (IS_VALID_COOR(i1 + 1, j1 + 1) && p[i1 + 1][j1 + 1] == 1) {
+					cur_len++;
+					i1++;
+					j1++;
+				}
+
+				if (cur_len > max_len1) {
+					max_len1 = cur_len;
+					ret1 = tmp;
+				}
+
+				tmp = RIGHT_UP;
+				cur_len = 1;
+				i1 = i; j1 = j;
+				while (IS_VALID_COOR(i1 - 1, j1 + 1) && p[i1 - 1][j1 + 1] == 1) {
+					cur_len++;
+					i1--;
+					j1++;
+				}
+
+				if (cur_len > max_len1) {
+					max_len1 = cur_len;
+					ret1 = tmp;
+				}
+
+			}
+
+			if (max_len1 > max_len) {
+				max_len = max_len1;
+				ret = ret1;
+				*get_line = i;
+				*get_row = j;
+				*len = max_len;
+			}
+		}
+	}
+
+	return ret;
+}
+
+/*
+ * x[out] : line
+ * y[out] : row
+ *
+ */
+int get_next_coordinate(const char (*base)[ROW_NUM], char (*change)[ROW_NUM], int *x, int *y)
+{
+	piece_direction dire;
+	int co_line, co_row, len;
+	int i, j;
+
+	dire = get_next_piece_direction(base, &co_row, &co_line, &len);
+	printf("=== dire %x line %d row %d =========\n", dire, co_line, co_row);
+
+	/*  当定下一个方向后，会有两种填棋子的方法，例如，方向为右，坐标为 (1,1)，假设长度为 1
+	 *  则会有两种选择，填在左边(0, 1)，填在右边(2, 1)，这里设置默认是先填的右边(2, 1)，这个后面改进
+	 */
+	// 以下的代码是基于 x 为横坐标， y 为纵坐标， 0,0 为坐标左上角
+	switch (dire) {
+	case RIGHT:
+
+		i = co_line; j = co_row + len;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+				goto right_ok;
+
+		i = co_line; j = co_row - 1;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+				goto right_ok;
+
+		// 放在自己的最小行且同行中最小列的棋子的旁边
+		for (i = 0; i < LINE_NUM; i++) {
+			for (j = 0; j < ROW_NUM; j++) {
+				if (change[i][j] == 1) {
+					if (IS_VALID_COOR(i, j + 1) && chess_map[i][j + 1] == 0) {
+						i = i; j = j + 1;
+						goto right_ok;
+					} else if (IS_VALID_COOR(i + 1, j) && chess_map[i + 1][j] == 0) {
+						i = i + 1; j = j;
+						goto right_ok;
+					} else if (IS_VALID_COOR(i, j - 1) && chess_map[i][j -1] == 0) {
+						i = i; j = j - 1;
+						goto right_ok;
+					} else if (IS_VALID_COOR(i - 1, j) && chess_map[i -1 ][j] == 0) {
+						i = i - 1; j = j;
+						goto right_ok;
+					}
+				}
+			}
+		}
+right_ok:
+		*x = i;
+		*y = j;
+		break;
+
+	case DOWN:
+
+		i = co_line + len; j = co_row;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto down_ok;
+
+		i = co_line - 1; j = co_row;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto down_ok;
+
+		for (i = 0; i < LINE_NUM; i++) {
+			for (j = 0; j < ROW_NUM; j++) {
+				if (change[i][j] == 1) {
+					if (IS_VALID_COOR(i, j + 1) && chess_map[i][j + 1] == 0) {
+						i = i; j = j + 1;
+						goto down_ok;
+					} else if (IS_VALID_COOR(i + 1, j) && chess_map[i + 1][j] == 0) {
+						i = i + 1; j = j;
+						goto down_ok;
+					} else if (IS_VALID_COOR(i, j - 1) && chess_map[i][j -1] == 0) {
+						i = i; j = j - 1;
+						goto down_ok;
+					} else if (IS_VALID_COOR(i - 1, j) && chess_map[i -1 ][j] == 0) {
+						i = i - 1; j = j;
+						goto down_ok;
+					}
+				}
+			}
+		}
+down_ok:
+		*x = i;
+		*y = j;
+		break;
+
+	case RIGHT_DOWN:
+
+		i = co_line + len; j = co_row + len;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto right_down_ok;
+
+		i = co_line - 1; j = co_row - 1;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto right_down_ok;
+
+		for (i = 0; i < LINE_NUM; i++) {
+			for (j = 0; j < ROW_NUM; j++) {
+				if (change[i][j] == 1) {
+					if (IS_VALID_COOR(i, j + 1) && chess_map[i][j + 1] == 0) {
+						i = i; j = j + 1;
+						goto right_down_ok;
+					} else if (IS_VALID_COOR(i + 1, j) && chess_map[i + 1][j] == 0) {
+						i = i + 1; j = j;
+						goto right_down_ok;
+					} else if (IS_VALID_COOR(i, j - 1) && chess_map[i][j -1] == 0) {
+						i = i; j = j - 1;
+						goto right_down_ok;
+					} else if (IS_VALID_COOR(i - 1, j) && chess_map[i -1 ][j] == 0) {
+						i = i - 1; j = j;
+						goto right_down_ok;
+					}
+				}
+			}
+		}
+
+right_down_ok:
+		*x = i;
+		*y = j;
+		break;
+
+	case RIGHT_UP:
+
+		i = co_line - len; j = co_row + len;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto right_up_ok;
+
+		i = co_line + 1; j = co_row - 1;
+		if (IS_VALID_COOR(i, j) && chess_map[i][j] == 0)
+			goto right_up_ok;
+
+		for (i = 0; i < LINE_NUM; i++) {
+			for (j = 0; j < ROW_NUM; j++) {
+				if (change[i][j] == 1) {
+					if (IS_VALID_COOR(i, j + 1) && chess_map[i][j + 1] == 0) {
+						i = i; j = j + 1;
+						goto right_up_ok;
+					} else if (IS_VALID_COOR(i + 1, j) && chess_map[i + 1][j] == 0) {
+						i = i + 1; j = j;
+						goto right_up_ok;
+					} else if (IS_VALID_COOR(i, j - 1) && chess_map[i][j -1] == 0) {
+						i = i; j = j - 1;
+						goto right_up_ok;
+					} else if (IS_VALID_COOR(i - 1, j) && chess_map[i -1 ][j] == 0) {
+						i = i - 1; j = j;
+						goto right_up_ok;
+					}
+				}
+			}
+		}
+right_up_ok:
+		*x = i;
+		*y = j;
+		break;
+
+	default:
+		printf("error happed in %s %d\n", __func__, __LINE__);
+		break;
+	}
+
+	printf("%s, get line %d row %d\n", __func__, *x, *y);
 
 	return 0;
 }
+
 /*
  * return : 1 : win, 0 : not win
  */
@@ -89,9 +335,9 @@ int is_win(int is_black)
 						goto win;
 				}
 
-				if (j >= 4 && (direct & RIGHT)) {
+				if (i >= 4 && (direct & RIGHT)) {
 					direct |= RIGHT_UP;
-					if (p[i][j] && p[i + 1][j - 1] && p[i + 2][j - 2] && p[i + 3][j - 3] && p[i + 4][j - 4])
+					if (p[i][j] && p[i - 1][j + 1] && p[i - 2][j + 2] && p[i - 3][j + 3] && p[i - 4][j + 4])
 						goto win;
 				}
 
@@ -205,10 +451,18 @@ static void draw_start(GtkWidget *draw,GdkEventButton *event,RECT_POINTER *rect)
 	GdkColor color;
 	static GdkGC *gc, *gc_black, *gc_white, *gc_red;
 	static int inited;
+#if 1
 	static int is_black = 1;
+#endif
 	char *black_win = "Congratulation , black is win";
 	char *white_win = "Congratulation , white is win";
+	int this_x, this_y;
 	int x, y;
+
+
+	static GdkFont *font = NULL;
+	if (!font)
+		font = gdk_font_load ("-misc-fixed-medium-r-*-*-*-140-*-*-*-*-*-*");
 
 	if (inited == 0) {
 		gc_black = gdk_gc_new(draw->window);
@@ -230,10 +484,12 @@ static void draw_start(GtkWidget *draw,GdkEventButton *event,RECT_POINTER *rect)
 		gdk_gc_set_rgb_fg_color(gc_red, &color);
 	}
 
+#if 0
 	if (is_black)
 		gc = gc_black;
 	else
 		gc = gc_white;
+#endif
 
 	// 灵敏
 	if (((int)event->x % 20) <= 10 && ((int)event->y % 20) <= 10)
@@ -247,15 +503,28 @@ static void draw_start(GtkWidget *draw,GdkEventButton *event,RECT_POINTER *rect)
 	} else
 		x = y = 0;
 
-	static GdkFont *font = NULL;
-	if (!font)
-		font = gdk_font_load ("-misc-fixed-medium-r-*-*-*-140-*-*-*-*-*-*");
+	if (chess_map[TO_LINE(y)][TO_ROW(x)] == 1)
+		return ;
+	else
+		chess_map[TO_LINE(y)][TO_ROW(x)] = 1;
 
-	printf("================ line %d row %d ===================\n", TO_LINE(y), TO_ROW(x));
+	printf("================ line %d row %d black %d===================\n", TO_LINE(y), TO_ROW(x), is_black);
 
-	gdk_draw_arc(draw->window, gc, TRUE, x - 10, y - 10, 20, 20, 0, 360 * 64);
+	gdk_draw_arc(draw->window, gc_black, TRUE, x - 10, y - 10, 20, 20, 0, 360 * 64);
+	chess_black[TO_LINE(y)][TO_ROW(x)] = 1;
+	if (is_win(1))
+		gdk_draw_text(draw->window, font, gc_red, 80, 160, black_win, strlen(black_win));
 
-	chess_map[TO_LINE(y)][TO_ROW(x)] = 1;
+	get_next_coordinate(chess_black, chess_white, &this_x, &this_y);
+	this_x = (this_x + 1) * 20;
+	this_y = (this_y + 1) * 20;
+	gdk_draw_arc(draw->window, gc_white, TRUE, this_y - 10, this_x - 10, 20, 20, 0, 360 * 64);
+	chess_map[TO_LINE(this_x)][TO_ROW(this_y)] = 1;
+	chess_white[TO_LINE(this_x)][TO_ROW(this_y)] = 1;
+	if (is_win(0))
+		gdk_draw_text(draw->window, font, gc_red, 80, 160, white_win, strlen(black_win));
+
+#if 0
 	if (is_black) {
 		chess_black[TO_LINE(y)][TO_ROW(x)] = 1;
 		if (is_win(1)) {
@@ -268,8 +537,8 @@ static void draw_start(GtkWidget *draw,GdkEventButton *event,RECT_POINTER *rect)
 		}
 	}
 
-
 	is_black = !is_black;
+#endif
 }
 
 static gboolean my_expose(GtkWidget      *da,
--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <draw.h>
 
-int test_draw(int argc,char **argv);
-int new_test( int argc, char *argv[]);
-
 int main(int argc, const char *argv[])
 {
-//	new_test(argc, argv);
 	test();
-//	test_draw(argc, argv);
 #ifdef WUZIQI
 	wuziqi();
 #else
